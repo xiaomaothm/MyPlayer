a55fd29e86d991a7e1bd6c70d19918b8d3701f63
xiaomaothm <283537004@qq.com>
2016-08-14 14:33:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/xiaomao/myplayer/MainActivity.java
@@ -0,0 +1,99 @@
+package com.example.xiaomao.myplayer;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import tcking.github.com.giraffeplayer.GiraffePlayer;
+import tv.danmaku.ijk.media.player.IMediaPlayer;
+
+public class MainActivity extends AppCompatActivity {
+    private GiraffePlayer player;
+    private String url="http://112.253.22.157/17/z/z/y/u/zzyuasjwufnqerzvyxgkuigrkcatxr/hc.yinyuetai.com/D046015255134077DDB3ACA0D7E68D45.flv";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        player = new GiraffePlayer(this);
+        player.play(url);
+        player.setTitle("音乐欣赏");
+        player.onComplete(new Runnable() {
+            @Override
+            public void run() {
+                //callback when video is finish
+                Toast.makeText(getApplicationContext(), "video play completed",Toast.LENGTH_SHORT).show();
+            }
+        }).onInfo(new GiraffePlayer.OnInfoListener() {
+            @Override
+            public void onInfo(int what, int extra) {
+                switch (what) {
+                    case IMediaPlayer.MEDIA_INFO_BUFFERING_START:
+                        //do something when buffering start
+                        break;
+                    case IMediaPlayer.MEDIA_INFO_BUFFERING_END:
+                        //do something when buffering end
+                        break;
+                    case IMediaPlayer.MEDIA_INFO_NETWORK_BANDWIDTH:
+                        //download speed
+                       // ((TextView) findViewById(R.id.tv_speed)).setText(Formatter.formatFileSize(getApplicationContext(),extra)+"/s");
+                        break;
+                    case IMediaPlayer.MEDIA_INFO_VIDEO_RENDERING_START:
+                        //do something when video rendering
+                       // findViewById(R.id.tv_speed).setVisibility(View.GONE);
+                        break;
+                }
+            }
+        }).onError(new GiraffePlayer.OnErrorListener() {
+            @Override
+            public void onError(int what, int extra) {
+                Toast.makeText(getApplicationContext(), "video play error",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (player != null) {
+            player.onPause();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if (player != null) {
+            player.onResume();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (player != null) {
+            player.onDestroy();
+        }
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (player != null) {
+            player.onConfigurationChanged(newConfig);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (player != null && player.onBackPressed()) {
+            return;
+        }
+        super.onBackPressed();
+    }
+}
